4a3057d873457eb1133cc565121523961a27eab7
celt95 <100221513+celt95@users.noreply.github.com>
2022-07-19 19:35:35 +0300

    Created main weather icon component

---
+++ src/MainWeatherIcon.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function MainWeatherIcon() {
+  return "hello from weather icon";
+}
--- src/WeatherResult.js
+++ src/WeatherResult.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import MainWeatherIcon from "./MainWeatherIcon";
 
 export default function WeatherResult(props) {
   return (
@@ -28,6 +29,7 @@ export default function WeatherResult(props) {
           </div>
         </div>
         <div className="col-6">
+          <MainWeatherIcon />
           <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" />
         </div>
       </div>
